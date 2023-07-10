2f8b84aee6400be0a39000615c41fa61bed55a19
Stephens37 <braydenstephens@protonmail.com>
2023-07-10 16:20:22 -0500

    'Step one complete'

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameBoard = (() => {
+  const boardArray = []
+  return boardArray
+})()
+
+const displayController = (() => {
+    
+})()
+
+const player = (xoro, suit) => {
+    const getXoro = () => xoro
+    const getSuit = () => suit
+    return { getXoro, getSuit }
+}
+
+
