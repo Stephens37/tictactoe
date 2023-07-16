29ae548316db326661b4e9f163402ebac10f52c8
Stephens37 <braydenstephens@protonmail.com>
2023-07-16 08:08:52 -0500

    'Work on repairing errors in selection function'

--- script.js
+++ script.js
@@ -9,26 +9,33 @@ const player = (suit, who) => {
   return {getSuit, getWho}
 }
 
-const displayController = ((controller, enemy) => {
-  if(document.getElementById("x") = true) {
-    controller = player.getSuit("x")
-    enemy = player.getSuit("y")
+const xChoice = document.getElementById('x_choice')
+const oChoice = document.getElementById('o_choice')
+
+const playerChoice = ((controller, enemy) => {
+  if (xChoice === true) {
+    controller = player.getSuit('x')
+    enemy = player.getSuit('y')
+  } else if (oChoice === true) {
+    controller = player.getSuit('o')
+    enemy = player.getSuit('x')
   }
-  else if (document.getElementById("o") = true) {
-    controller = player.getSuit("o")
-    enemy = player.getSuit("x")
-  }
+  console.log(controller)
+  return { controller, enemy }
+})
+
+xChoice.addEventListener(onclick, playerChoice)
+oChoice.addEventListener(onclick, playerChoice)
+
+// if player chooses x then player will equal const x and computer will equal const o
+// else player equals 
+/* const whichPlayer = (controller, enemy) => 
+if x is pressed {
+  player(x, controller)
+  player(o, enemy)
+  else
+  player(o, controller)
+  player(x, enemy)
+}
 return {controller, enemy}
-})()
-  // if player chooses x then player will equal const x and computer will equal const o
-  // else player equals 
-  /*const whichPlayer = (controller, enemy) => 
-  if x is pressed {
-    player(x, controller)
-    player(o, enemy)
-    else
-    player(o, controller)
-    player(x, enemy)
-  }
-  return {controller, enemy}
-})()
+})() */
