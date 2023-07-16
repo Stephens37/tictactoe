9d1d37018640868cb73d442dcb9ee9cef8a844e4
Stephens37 <braydenstephens@protonmail.com>
2023-07-16 02:21:42 -0500

    'Work on creating function to choose x or o'

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const gameBoard = (() => {
   return boardArray
 })()
 
-const displayController = (() => {
-    
-})()
-
-const player = (xoro, suit) => {
-    const getXoro = () => xoro
-    const getSuit = () => suit
-    return { getXoro, getSuit }
+const player = (suit, who) => {
+  const getSuit = () => suit
+  const getWho = () => who
+  return {getSuit, getWho}
 }
 
-
+const displayController = (() => {
+  // if player chooses x then player will equal const x and computer will equal const o
+  // else player equals 
+  /*const whichPlayer = (controller, enemy) => 
+  if x is pressed {
+    player(x, controller)
+    player(o, enemy)
+    else
+    player(o, controller)
+    player(x, enemy)
+  }
+  return {controller, enemy}
+})()
