d9cea6dfa028ee7e1c3a6cb758dbec3b1b452183
Stephens37 <braydenstephens@protonmail.com>
2023-07-16 04:48:40 -0500

    'Create function for selecting x or o'

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ const player = (suit, who) => {
   return {getSuit, getWho}
 }
 
-const displayController = (() => {
+const displayController = ((controller, enemy) => {
+  if(document.getElementById("x") = true) {
+    controller = player.getSuit("x")
+    enemy = player.getSuit("y")
+  }
+  else if (document.getElementById("o") = true) {
+    controller = player.getSuit("o")
+    enemy = player.getSuit("x")
+  }
+return {controller, enemy}
+})()
   // if player chooses x then player will equal const x and computer will equal const o
   // else player equals 
   /*const whichPlayer = (controller, enemy) => 
