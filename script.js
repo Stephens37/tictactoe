554b5f87e03fd7044c7aeb3fefe76a9fec8b02a2
Stephens37 <braydenstephens@protonmail.com>
2023-07-23 12:01:24 -0500

    'Work on getting x/o to appear'

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
-const xChoice = document.getElementById('x_choice')
-const oChoice = document.getElementById('o_choice')
-
-const player = (playerChoice) => {
-  if (xChoice === true) {
-    playerChoice.innerHTML = 'x'
-  } else if (oChoice === true) {
-    playerChoice.innerHTML = 'o'
-  }
-  return playerChoice
-}
-
-xChoice.addEventListener(onclick, player)
-oChoice.addEventListener(onclick, player)
-
-const enemy = (enemyChoice, controller) => {
-  if (controller.innerHTML === 'x') {
-    enemyChoice.innerHTML = 'o'
-  } else if (controller.innerHTML === 'o') {
-    enemyChoice.innerHTML = 'x'
-  }
-  return enemyChoice
-}
-
-xChoice.addEventListener(onclick, enemy)
-oChoice.addEventListener(onclick, enemy)
-
 const gameBoard = (() => {
-  const boardArray = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']
-  const square = document.getElementsByClassName('square')
-  return { boardArray, square }
-})()
+  const rows = 3
+  const columns = 3
+  const board = []
 
-// if player chooses x then player will equal const x and computer will equal const o
-// else player equals 
-/* const whichPlayer = (controller, enemy) => 
-if x is pressed {
-  player(x, controller)
-  player(o, enemy)
-  else
-  player(o, controller)
-  player(x, enemy)
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board
+
+  const printBoard = (() => {
+    const square = document.getElementsByClassName('square')
+    const boardWithSquareValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithSquareValues);
+  })()
+return { getBoard, printBoard }
 }
-return {controller, enemy}
-})() */
+
+
+function Cell() {
+  let value = ''
+  const addMark = (player) => {
+    value = player
+  }
+
+  const getValue = () => value
+
+  return {
+    addMark,
+    getValue
+  };
+}
+
+const gameFlow = () => {
+const getPlayerTurn = ((playerTurn) => {
+  for (playerTurn = 1; playerTurn < gameflow.length; playerTurn++) {
+    if (playerTurn % 2 === 0) {
+      player = o
+      document.getElementByClass('turn').innerHTML = "O's turn"
+    }
+    else {
+      player = 'x'
+      document.getElementByClass('turn').innerHTML = "X's turn"
+  }
+}
+})()
+}
+
+/* const player = () => {
+  
+}
+
+const gameFlow = (() => {
+
+}) */
