a825be5675a78f4b3f78103432baee6955db68f9
Stephens37 <braydenstephens@protonmail.com>
2023-07-24 20:05:30 -0500

    'Work on showing x/o"

--- script.js
+++ script.js
@@ -4,50 +4,58 @@ const gameBoard = (() => {
   const board = []
 
   for (let i = 0; i < rows; i++) {
-    board[i] = [];
+    board[i] = []
     for (let j = 0; j < columns; j++) {
-      board[i].push(Cell());
+      board[i].push(Cell())
     }
   }
 
   const getBoard = () => board
 
   const printBoard = (() => {
-    const square = document.getElementsByClassName('square')
     const boardWithSquareValues = board.map((row) => row.map((cell) => cell.getValue()))
-    console.log(boardWithSquareValues);
+    console.log(boardWithSquareValues)
   })()
-return { getBoard, printBoard }
-}
+  return { getBoard, printBoard }
+})()
 
-
-function Cell() {
+function Cell () {
   let value = ''
   const addMark = (player) => {
     value = player
+  }
+  const printCells = (cell) => {
+    cell = document.createElement('button')
+    cell.className = 'cell'
+    cell.append(document.getElementsByClassName('gameboard'))
+    return cell
   }
 
   const getValue = () => value
 
   return {
     addMark,
-    getValue
-  };
-}
-
-const gameFlow = () => {
-const getPlayerTurn = ((playerTurn) => {
-  for (playerTurn = 1; playerTurn < gameflow.length; playerTurn++) {
-    if (playerTurn % 2 === 0) {
-      player = o
-      document.getElementByClass('turn').innerHTML = "O's turn"
-    }
-    else {
-      player = 'x'
-      document.getElementByClass('turn').innerHTML = "X's turn"
+    getValue,
+    printCells
   }
 }
-})()
+
+const gameFlow = (board, player) => {
+  const getPlayerTurn = ((playerTurn) => {
+    for (playerTurn = 1; playerTurn < board.length; playerTurn++) {
+      if (playerTurn % 2 === 0) {
+        player = 'o'
+        document.getElementByClass('turn').innerHTML = "O's turn"
+      }
+      else {
+        player = 'x'
+        document.getElementByClass('turn').innerHTML = "X's turn"
+      }
+    }
+  })()
+
+
+  return { getPlayerTurn }
 }
 
 /* const player = () => {
