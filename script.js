ed612998878eb59ce8c3da457b538438fc84bb88
Stephens37 <braydenstephens@protonmail.com>
2023-07-30 18:05:47 -0500

    'Work on changing nodevalue upon click'

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ const board = [
   'x', 'o', 'x'
 ]
 
+const gameFlow = (player) => {
+  const getPlayerTurn = (playerTurn) => {
+    for (playerTurn = 1; playerTurn < board.length; playerTurn++) {
+      if (playerTurn % 2 === 0) {
+        player = 'o'
+        this.document.querySelector('.xobuttons').nodeValue = 'o'
+      } else {
+        player = 'x'
+        this.document.querySelector('.xobuttons').nodeValue = 'x'
+      }
+    }
+  }
+  return { getPlayerTurn }
+}
+
 function gameButtons () {
   for (i = 0; i < board.length; i++) {
     const xobuttons = document.createElement('button')
@@ -13,24 +28,7 @@ function gameButtons () {
     const t = document.createTextNode(board[i])
     xobuttons.appendChild(t)
     gameBoard.appendChild(xobuttons)
+    xobuttons.addEventListener('click', gameFlow)
   }
 }
 window.onload = gameButtons()
-
-const gameFlow = (board, player) => {
-  const getPlayerTurn = ((playerTurn) => {
-    for (playerTurn = 1; playerTurn < board.length; playerTurn++) {
-      if (playerTurn % 2 === 0) {
-        player = 'o'
-        document.getElementByClass('turn').innerHTML = "O's turn"
-      }
-      else {
-        player = 'x'
-        document.getElementByClass('turn').innerHTML = "X's turn"
-      }
-    }
-  })()
-
-
-  return { getPlayerTurn }
-}
