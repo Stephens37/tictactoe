c15a26b9c8c102fbd7c70022a6258c64eb574dd4
Stephens37 <braydenstephens@protonmail.com>
2023-08-01 19:06:48 -0500

    'Work on changing button text when clicked'

--- script.js
+++ script.js
@@ -1,50 +1,38 @@
 const gameBoard = document.querySelector('.gameboard')
-
-const board = [
-  'x', 'o', 'x',
+const buttons = document.querySelector('.xobuttons')
+let board = [
+  buttons.getElementByID('button1'), 'o', 'x',
   'o', 'x', 'o',
   'x', 'o', 'x'
 ]
 
-const gameFlow = () => {
-  const getPlayerTurn = () => {
-    for (i = 0; i < board.length; i++) {
-      if (i % 2 === 0) {
-        let button = document.querySelector('.xobuttons')
-        button.addEventListener('click', function (event) {
-          let num = parseInt(event.target.dataset.key)
-          board.splice(num, 0, 'x')
-          const t = document.createTextNode(board[i])
-          button.appendChild(t)
-          gameBoard.appendChild(button)
-        })
-      } else {
-        let button = document.querySelector('.xobuttons')
-        button.addEventListener('click', function (event) {
-          let num = parseInt(event.target.dataset.key)
-          board.splice(num, 0, 'o')
-          const t = document.createTextNode(board[i])
-          button.appendChild(t)
-          gameBoard.appendChild(button)
-        })
-      }
+function playerXO (player) {
+  let playerTurn = 0
+  for (playerTurn = 0; playerTurn < 9; playerTurn++) {
+    if (playerTurn % 2 === 0) {
+      player = 'x'
+    } else if (playerTurn % 2 === 1) {
+      player = 'o'
     }
   }
-  getPlayerTurn()
+  return player
 }
-gameFlow()
 
- /* function gameButtons () {
+const gameFlow = () => {
+  const button = document.querySelector('.xobuttons')
+  button.addEventListener('click', function (event) {
+    const num = parseInt(event.target.dataset.key)
+    board.splice(num, 0, playerXO())
+  })
+}
+window.onload = gameFlow()
+
+function gameButtons () {
   for (i = 0; i < board.length; i++) {
     const t = document.createTextNode(board[i])
+    let xobuttons = document.querySelector('.xobuttons')
     xobuttons.appendChild(t)
     gameBoard.appendChild(xobuttons)
-    xobuttons.addEventListener('click', gameFlow)
   }
 }
-window.onload = gameButtons() 
-
- document.addEventListener('click', function (event) {
-  let num = parseInt(event.target.dataset.key)
-  board.splice(num, 0, 'x')
-}) */
+window.onload = gameButtons()
