eab2b2289aea8b769cdc1ea9e802cef53d747bd4
Stephens37 <braydenstephens@protonmail.com>
2023-08-06 03:54:24 -0500

    'Simplify javascript code'

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const gameBoard = document.querySelector('.gameboard')
-const buttons = document.querySelector('.xobuttons')
 let board = []
 
 const playerXO = () => {
@@ -14,23 +12,12 @@ const playerXO = () => {
   return player
 }
 
-const gameFlow = () => {
+const gameFlow = (() => {
   const button = document.querySelector('.gameboard')
   button.addEventListener('click', function (event) {
-    const num = event.target.textContent
-    board.splice(num, 0, playerXO())
+    board.push(playerXO())
     event.target.textContent = playerXO()
     console.log(playerXO())
     event.target.disabled = true
   })
-}
-window.onload = gameFlow()
-
-const gameButtons = (() => {
-  for (i = 0; i < board.length; i++) {
-    const t = document.createTextNode(board[i])
-    let xobuttons = document.querySelector('.xobuttons')
-    xobuttons.appendChild(t)
-    gameBoard.appendChild(xobuttons)
-  }
 })()
