b84fbd28b66294076f825f032c677216d2ff2392
Stephens37 <braydenstephens@protonmail.com>
2023-08-06 02:01:37 -0500

    Add ability to add text to button

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const gameBoard = document.querySelector('.gameboard')
 const buttons = document.querySelector('.xobuttons')
 let board = [
-  buttons.getElementByID('button1'), 'o', 'x',
-  'o', 'x', 'o',
-  'x', 'o', 'x'
+  '', '', '',
+  '', '', '',
+  '', '', ''
 ]
 
-function playerXO (player) {
-  let playerTurn = 0
-  for (playerTurn = 0; playerTurn < 9; playerTurn++) {
-    if (playerTurn % 2 === 0) {
+const playerXO = () => {
+  let player = ''
+  for (i = 0; i < 9; i++) {
+    if (i % 2 === 0) {
       player = 'x'
-    } else if (playerTurn % 2 === 1) {
+    } else if (i % 2 === 1) {
       player = 'o'
     }
   }
@@ -20,9 +20,13 @@ function playerXO (player) {
 
 const gameFlow = () => {
   const button = document.querySelector('.xobuttons')
+  const igrok = playerXO()
   button.addEventListener('click', function (event) {
-    const num = parseInt(event.target.dataset.key)
-    board.splice(num, 0, playerXO())
+    playerXO()
+    const num = event.target.textContent
+    board.splice(num, 0, igrok)
+    event.target.textContent = igrok
+    console.log(igrok)
   })
 }
 window.onload = gameFlow()
