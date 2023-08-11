8f0e9bbfecae97fdd73e1d1ae5fac6b0dd97efe2
Stephens37 <braydenstephens@protonmail.com>
2023-08-11 08:50:31 -0500

    'Decide to just keep x/o as names'

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const button8 = document.querySelector('#button8')
 
 let board = []
 
-const form = document.getElementById('xo_form')
-
 const playerXO = () => {
   let player = ''
   for (i = 0; i < board.length; ++i) {
@@ -24,29 +22,48 @@ const playerXO = () => {
   return player
 }
 
-const submitForm = () => {
-  const playerArray = []
-  const playerX = document.querySelector('#name1').innerHTML
-  const playerO = document.querySelector('#name2').innerHTML
-  playerArray.push(playerX, playerO)
-  console.log(playerX)
-  console.log(playerArray)
-}
-
-form.addEventListener('submit', submitForm)
-
 const gameFlow = (() => {
-  const playerForm = document.querySelector('#xo_form')
   const button = document.querySelector('.gameboard')
   button.addEventListener('click', function (event) {
-    playerForm.style.display = 'none'
     board.push(playerXO())
     event.target.textContent = playerXO()
     console.log(playerXO())
     event.target.disabled = true
+    const bottomGrid = document.querySelector('#bottom_grid')
+    const winner = document.createElement('div')
+    winner.setAttribute('id', 'winner')
+    bottomGrid.append(winner)
+
+    const afterWin = () => {
+      const xobuttons = document.querySelectorAll('.xobuttons')
+      for (let i = 0; i < xobuttons.length; i++) {
+        xobuttons[i].disabled = false
+      }
+      const resetButton = document.createElement('button')
+      resetButton.setAttribute('id', 'reset_button')
+      resetButton.innerText = 'Reset'
+      resetButton.style.textAlign = 'center'
+      bottomGrid.append(resetButton)
+      resetButton.addEventListener('click', function () {
+        winner.remove()
+        resetButton.remove()
+        board.length = 0
+        const buttonTextReset = (() => {
+          button0.textContent = ''
+          button1.textContent = ''
+          button2.textContent = ''
+          button3.textContent = ''
+          button4.textContent = ''
+          button5.textContent = ''
+          button6.textContent = ''
+          button7.textContent = ''
+          button8.textContent = ''
+        })()
+        return buttonTextReset
+      })
+    }
 
     const winFunction = (() => {
-      const bottomGrid = document.querySelector('#bottom_grid')
       if ((button0.textContent === 'x' && button1.textContent === 'x' && button2.textContent === 'x') ||
       (button3.textContent === 'x' && button4.textContent === 'x' && button5.textContent === 'x') ||
       (button6.textContent === 'x' && button7.textContent === 'x' && button8.textContent === 'x') ||
@@ -55,106 +72,21 @@ const gameFlow = (() => {
       (button2.textContent === 'x' && button5.textContent === 'x' && button8.textContent === 'x') ||
       (button0.textContent === 'x' && button4.textContent === 'x' && button8.textContent === 'x') ||
       (button2.textContent === 'x' && button4.textContent === 'x' && button6.textContent === 'x')) {
-        /* const xobuttons = document.querySelector('.xobuttons')
-        xobuttons.disabled = true */
-        const winner = document.createElement('div')
-        winner.setAttribute('id', 'winner')
-        bottomGrid.append(winner)
-        
-
-        const resetButton = document.createElement('button')
-        resetButton.setAttribute('id', 'reset_button')
-        resetButton.innerText = 'Reset'
-        bottomGrid.append(resetButton)
-
-        resetButton.addEventListener('click', function () {
-          winner.remove()
-          resetButton.remove()
-          board.length = 0
-          playerForm.style.display = 'grid'
-          const buttonTextReset = (() => {
-            button0.textContent = ''
-            button1.textContent = ''
-            button2.textContent = ''
-            button3.textContent = ''
-            button4.textContent = ''
-            button5.textContent = ''
-            button6.textContent = ''
-            button7.textContent = ''
-            button8.textContent = ''
-          })()
-          return buttonTextReset
-        })
-
-        return resetButton
-      } else if ((button0.textContent === 'x' && button1.textContent === 'x' && button2.textContent === 'x') ||
-      (button3.textContent === 'x' && button4.textContent === 'x' && button5.textContent === 'x') ||
-      (button6.textContent === 'x' && button7.textContent === 'x' && button8.textContent === 'x') ||
-      (button0.textContent === 'x' && button3.textContent === 'x' && button6.textContent === 'x') ||
-      (button1.textContent === 'x' && button4.textContent === 'x' && button7.textContent === 'x') ||
-      (button2.textContent === 'x' && button5.textContent === 'x' && button8.textContent === 'x') ||
-      (button0.textContent === 'x' && button4.textContent === 'x' && button8.textContent === 'x') ||
-      (button2.textContent === 'x' && button4.textContent === 'x' && button6.textContent === 'x')) {
-        const winner = document.createElement('div')
-        winner.setAttribute('id', 'winner')
-        bottomGrid.append(winner)
-        winner.textContent = '{$name2} wins'
-
-        const resetButton = document.createElement('button')
-        resetButton.setAttribute('id', 'reset_button')
-        resetButton.innerText = 'Reset'
-        bottomGrid.append(resetButton)
-
-        resetButton.addEventListener('click', function () {
-          winner.remove()
-          resetButton.remove()
-          board.length = 0
-          playerForm.style.display = 'grid'
-          const buttonTextReset = (() => {
-            button0.textContent = ''
-            button1.textContent = ''
-            button2.textContent = ''
-            button3.textContent = ''
-            button4.textContent = ''
-            button5.textContent = ''
-            button6.textContent = ''
-            button7.textContent = ''
-            button8.textContent = ''
-          })()
-          return buttonTextReset
-        })
-
-        return resetButton
+        winner.textContent = 'X wins'
+        afterWin()
+      } else if ((button0.textContent === 'o' && button1.textContent === 'o' && button2.textContent === 'o') ||
+      (button3.textContent === 'o' && button4.textContent === 'o' && button5.textContent === 'o') ||
+      (button6.textContent === 'o' && button7.textContent === 'o' && button8.textContent === 'o') ||
+      (button0.textContent === 'o' && button3.textContent === 'o' && button6.textContent === 'o') ||
+      (button1.textContent === 'o' && button4.textContent === 'o' && button7.textContent === 'o') ||
+      (button2.textContent === 'o' && button5.textContent === 'o' && button8.textContent === 'o') ||
+      (button0.textContent === 'o' && button4.textContent === 'o' && button8.textContent === 'o') ||
+      (button2.textContent === 'o' && button4.textContent === 'o' && button6.textContent === 'o')) {
+        winner.textContent = 'O wins'
+        afterWin()
       } else if (board.length === 9) {
-        const winner = document.createElement('div')
-        winner.setAttribute('id', 'winner')
-        bottomGrid.append(winner)
         winner.textContent = 'Tie game'
-
-        const resetButton = document.createElement('button')
-        resetButton.setAttribute('id', 'reset_button')
-        resetButton.innerText = 'Reset'
-        bottomGrid.append(resetButton)
-
-        resetButton.addEventListener('click', function () {
-          winner.remove()
-          resetButton.remove()
-          playerForm.style.display = 'grid'
-          const buttonTextReset = (() => {
-            button0.textContent = ''
-            button1.textContent = ''
-            button2.textContent = ''
-            button3.textContent = ''
-            button4.textContent = ''
-            button5.textContent = ''
-            button6.textContent = ''
-            button7.textContent = ''
-            button8.textContent = ''
-          })()
-          return buttonTextReset
-        })
-
-        return resetButton
+        afterWin()
       }
     })()
   })
